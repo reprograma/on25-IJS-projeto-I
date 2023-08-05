3f62e5c5d7584dd59f7bc6c4da188b40cff98275
brena-odwyer <odwyer.brena@gmail.com>
2023-08-05 09:23:34 -0400

    class Account

---
+++ exercicios_projeto/Account/Account.js
@@ -0,0 +1,34 @@
+class Account extends Client {
+    id
+    name
+    cpf
+    rendaMensal
+    saldo
+
+    constructor(id, name, cpf, rendaMensal, saldo){
+        super(id, name, cpf, rendaMensal, saldo)
+    }
+
+
+
+
+    cadastrarPix(){
+
+    }
+
+    sacar(){
+
+    }
+
+    transferir(){
+
+    }
+
+    pix(){
+
+    }
+
+    depositar(){
+
+    }
+}
