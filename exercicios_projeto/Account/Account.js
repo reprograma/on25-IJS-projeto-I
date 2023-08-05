b2510cec6e391dae801b35b5dcacd2c0248656ab
hinale <leticiaamarallara@hotmail.com>
2023-08-05 11:48:25 -0300

    feat: class account

---
+++ exercicios_projeto/Account/Account.js
@@ -0,0 +1,38 @@
+class Account {
+  client;
+  #balance;
+  dailyLimit;
+
+  constructor(client) {
+    this.client = client;
+  }
+
+  get balance() {
+    return this.#balance
+  }
+
+  deposit(amount) {
+    this.#balance += amount;
+    return true
+  }
+
+  withdraw(amount) {
+    if(amount <= this.#balance || amount <= this.dailyLimit) {
+      this.#balance -= amount;
+      return true;
+    }
+
+    return false;
+  }
+
+  transferTo(anotherAccount, amount) {
+    if(this.withdraw(amount)) {
+      anotherAccount.deposit(amount);
+      return true
+    }
+
+    return false;
+  }
+}
+
+module.exports = Account
