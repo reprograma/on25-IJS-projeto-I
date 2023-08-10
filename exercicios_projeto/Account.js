c6bb120199b10ad36f265aeff17ea372e9d87576
Ellie <elliekonuma@gmail.com>
2023-08-10 16:38:16 -0300

    Creation method in Bank & refactor of methods Acc

--- exercicios_projeto/Account.js
+++ exercicios_projeto/Account.js
@@ -1,22 +1,23 @@
 const Client = require("./Client");
-const Bank = require("./Bank");
 
 class Account {
   #client;
   #bank;
   #balance;
-  #transitionThreshold;
-  #typeAcc;
-  #keyPix;
+  #transactionThreshold;
+  #accType;
+  #pixKeys;
+  static lowIncomeThreshold = 5000;
+  static highIncomeThreshold = 18000;
+
   constructor(client, bank) {
     this.#client = client;
     this.#bank = bank;
     this.#balance = 0;
-    this.#transitionThreshold;
-    this.#typeAcc;
-    this.#keyPix = [];
+    this.#pixKeys = { email: null, cpf: null, phone: null, random: [] };
 
-    this.constructor.checkTypeAcc();
+    this.#setAccType();
+    this.#bank.pushAcc(this);
   }
   get client() {
     return this.#client;
@@ -27,117 +28,178 @@ class Account {
   get balance() {
     return this.#balance;
   }
-  get transitionThreshold() {
-    return this.#transitionThreshold;
+  get transactionThreshold() {
+    return this.#transactionThreshold;
   }
+
   get typeAcc() {
-    return this.#typeAcc;
+    return this.#accType;
   }
-  get keyPix() {
-    return this.#keyPix;
+  get pixKeys() {
+    return this.#pixKeys;
   }
-  static checkTypeAcc() {
-    if (!(this.client instanceof Client)) {
-      return new Error("Não é uma cliente");
+
+  #setAccType() {
+    if (!(this.#client instanceof Client)) {
+      throw new Error("Não é uma Client");
     }
-    if (this.#client.monthlyIncome > 4999.9) {
-      this.#transitionThreshold = 1000;
-      this.typeAcc = "Standard";
+
+    if (this.#client.monthlyIncome < this.constructor.lowIncomeThreshold) {
+      this.#transactionThreshold = 1000;
+      this.#accType = "Standard";
     } else if (
-      this.#client.monthlyIncome <= 5000 &&
-      this.#client.monthlyIncome > 17999.9
+      this.#client.monthlyIncome >= this.constructor.lowIncomeThreshold &&
+      this.#client.monthlyIncome < this.constructor.highIncomeThreshold
     ) {
-      this.#transitionThreshold = 5000;
-      this.#typeAcc = "Gold";
-    } else if (this.#client.monthlyIncome <= 18000) {
-      this.#transitionThreshold = "unlimited";
-      this.#typeAcc = "Premium";
+      this.#transactionThreshold = 5000;
+      this.#accType = "Gold";
+    } else if (
+      this.#client.monthlyIncome >= this.constructor.highIncomeThreshold
+    ) {
+      this.#transactionThreshold = Number.POSITIVE_INFINITY;
+      this.#accType = "Premium";
     }
   }
   deposit(amount) {
-    this.#balance += amount;
-    console.log(`Foi depositado ${amount} e ficou ${this.#balance}`);
+    if (typeof amount === "string" || typeof amount === "boolean") {
+      throw new Error(
+        "Não é possivel depositar valores que nao sejam numeros inteiros"
+      );
+    }
+    if (amount > 0) {
+      this.#balance += amount;
+      console.log(`Foi depositado ${amount} e ficou ${this.#balance}`);
+      return "Foi feito o deposito!";
+    } else {
+      throw new Error("Não é possivel depositar valores negativos");
+    }
   }
   draw(amount) {
-    if (amount > this.#balance) {
-      return new Error("Erro valor amount maior do que o balance");
+    if (typeof amount === "string" || typeof amount === "boolean") {
+      throw new Error(
+        "Não é possivel retirar (sacar) valores que nao sejam numeros inteiros"
+      );
     }
-    this.#balance -= amount;
-    console.log(`Você sacou ${amount} e ficou ${this.#balance}`);
+
+    if (amount > this.#balance) {
+      throw new Error("Erro valor de retirada maior do que o balance");
+    }
+
+    if (amount > 0) {
+      this.#balance -= amount;
+      console.log(`Você sacou ${amount} e ficou ${this.#balance}`);
+      return "Saque feito!";
+    } else {
+      throw new Error("Não é possivel retirar (sacar) valores negativos");
+    }
   }
 
   tedTransfer(acc, cpf, amount) {
     if (!(acc instanceof Account)) {
-      return new Error("Erro Não é uma conta");
+      throw new Error("Erro Não é uma conta");
     }
     if (amount > this.#balance) {
-      return new Error("Erro valor amount maior do que o balance");
+      throw new Error("Erro valor amount maior do que o balance");
     }
-    if (amount > this.#transitionThreshold) {
+    if (amount > this.#transactionThreshold) {
       console.log(
-        `Valor ${amount} é mais do que o permitido diario.\\Valor permitido ${
-          this.#transitionThreshold
+        `Valor ${amount} é mais do que o permitido diario.\nValor permitido ${
+          this.#transactionThreshold
         }`
       );
-      return new Error("Erro valor amount maior do que o permitido");
+      throw new Error("Erro valor amount maior do que o permitido");
     }
     if (acc.#client.cpf != cpf) {
-      return new Error("Erro cpf não esta certo");
+      throw new Error("Erro cpf não esta certo");
     }
-    this.#transitionThreshold -= amount;
-    this.this.#balance -= amount;
-    acc.deposit(amount);
-    console.log("tranzição feita!");
-  }
-  createKeyPix(opition) {
-    let e = 0;
-    let p = 0;
-    let c = 0;
-    if (e == 1 || p == 1 || c == 1) {
-      console.log(`Você já criou com ${opition}`);
-      return new Error("Erro chave pix repetida");
-    }
-    if (opition == "email" && e == 0) {
-      e++;
-      this.keyPix.push(this.#client.email);
-    } else if (opition == "phone" && p == 0) {
-      p++;
-      this.keyPix.push(this.#client.phone);
-    } else if (opition == "cpf" && c == 0) {
-      c++;
-      this.keyPix.push(this.#client.cpf);
-    } else {
-      console.log("as opções são email, phone ou cpf");
-      return new Error("Erro de criaçao de chave pix");
-    }
-  }
-
-  pix(acc, key, amount) {
-    if (!(acc instanceof Account)) {
-      return new Error("Erro Não é uma conta");
-    }
-    if (amount > this.#balance) {
-      return new Error("Erro valor amount maior do que o balance");
-    }
-    if (amount > this.#transitionThreshold) {
-      console.log(
-        `Valor ${amount} é mais do que o permitido diario.\\Valor permitido ${
-          this.#transitionThreshold
-        }`
-      );
-      return new Error("Erro valor amount maior do que o permitido");
-    }
-    if (!(acc.#bank.bankCode == this.#bank.bankCode)) {
-      return new Error("Erro não é o mesmo banco");
-    }
-
-    if (!acc.keyPix.includes(key)) {
-      return new Error("Erro chave de pix errado");
-    }
-    this.#transitionThreshold -= amount;
+    this.#transactionThreshold -= amount;
     this.#balance -= amount;
     acc.deposit(amount);
-    console.log("Pix efetuado");
+    console.log("tranzição feita!");
+    return "Transferencia via TED feita";
+  }
+  createPixKey(option) {
+    switch (option) {
+      case "email":
+        if (this.#pixKeys.email == null) {
+          this.#pixKeys.email = this.#client.email;
+          console.log("Criação chave pix com email criada com sucesso!");
+          return "Criada chave pix";
+        } else {
+          console.log(`Você já criou com email`);
+          throw new Error("Erro chave pix repetida");
+        }
+        break;
+
+      case "phone":
+        if (this.#pixKeys.phone == null) {
+          this.#pixKeys.phone = this.#client.phone;
+          console.log("Criação chave pix com phone criada com sucesso!");
+          return "Criada chave pix";
+        } else {
+          console.log(`Você já criou com phone`);
+          throw new Error("Erro chave pix repetida");
+        }
+        break;
+
+      case "cpf":
+        if (this.#pixKeys.cpf == null) {
+          this.#pixKeys.cpf = this.#client.cpf;
+          console.log("Criação chave pix com cpf criada com sucesso!");
+          return "Criada chave pix";
+        } else {
+          console.log(`Você já criou com cpf`);
+          throw new Error("Erro chave pix repetida");
+        }
+        break;
+
+      case "random":
+        this.#pixKeys.random.push(
+          Math.fround(Date.now() * (1 + Math.random()))
+        );
+        console.log("Criação chave pix com random criada com sucesso!");
+        return "Criada chave pix";
+        break;
+
+      default:
+        console.log("as opções são email, phone ou cpf");
+        throw new Error("Erro de criaçao de chave pix");
+    }
+  }
+
+  pixTransfer(key, type, bank, amount) {
+    if (amount > this.#balance) {
+      throw new Error("Erro valor amount maior do que o balance");
+    }
+
+    if (amount > this.#transactionThreshold) {
+      console.log(
+        `Valor ${amount} é mais do que o permitido diario.\\Valor permitido ${
+          this.#transactionThreshold
+        }`
+      );
+      throw new Error("Erro valor amount maior do que o permitido");
+    }
+
+    const acc = bank.accs.filter((account) => {
+      if (type != "random") {
+        return account.pixKeys[type] == key;
+      } else {
+        return account.pixKeys[type].includes(key);
+      }
+    })[0];
+
+    // console.log(acc);
+
+    if (!(acc.#bank.bankCode == this.#bank.bankCode)) {
+      throw new Error("Erro não é o mesmo banco");
+    }
+
+    this.#transactionThreshold -= amount;
+    this.#balance -= amount;
+    acc.deposit(amount);
+    console.log("Pix efetuado!");
+    return "Pix efetuado";
   }
 }
 
--- exercicios_projeto/Bank.js
+++ exercicios_projeto/Bank.js
@@ -1,10 +1,15 @@
 class Bank {
   bankName;
   bankCode;
-
+  accs;
   constructor(name, code) {
     this.bankName = name;
     this.bankCode = code;
+    this.accs = [];
+  }
+
+  pushAcc(acc) {
+    this.accs.push(acc);
   }
 }
 
